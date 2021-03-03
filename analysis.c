9438d892236b8d4fc6f24dba72f0ae8b2b064055
Israel Botelho <israel.ifbj@gmail.com>
2021-03-03 00:28:53 -0300

    03/03

--- analysis.c
+++ analysis.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "path.h"
 
-void startAnalysis(char* pathOut){
-    
+void startAnalysis(char* pathOut, int numberVisit, int listSize){
+    char* fullPath = catPath(pathOut, "analysis.txt");
+    FILE *txt = fopen(fullPath, "w");
+
+    if(!txt){
+        printf("Relatorio nao foi possivel ser criado!");
+        free(fullPath);
+        return;
+    }
+
+    fprintf(txt, "size %d visit %d\n", listSize, numberVisit);    
+
+    fclose(txt);
+    free(fullPath);
+}
+
+void continueAnalysis(char* pathOut, int numberVisit, int listSize){
+    char* fullPath = catPath(pathOut, "analysis.txt");
+    FILE *txt = fopen(fullPath, "a");
+
+    if(!txt){
+        printf("Relatorio nao foi possivel ser continuado!");
+        free(fullPath);
+        return;
+    }
+
+    fprintf(txt, "size %d visit %d\n", listSize, numberVisit);    
+
+    fclose(txt);
+    free(fullPath);
 }
