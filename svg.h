f2410d01a5415cc3c17d846439792b31c2ba37a3
Israel Botelho <israel.ifbj@gmail.com>
2021-03-10 01:49:30 -0300

    10/03

--- svg.h
+++ svg.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include "rectangle.h"
+
 /*
 * Cria e Preenche um arquivo svg com os arquivos de uma lista passada para um diretorio de saida
 * Pre: Um void* com o endereço da lista, Um char* com a string do caminho de saida, Um char* com a string do nome do SVG, Um int com o swList
